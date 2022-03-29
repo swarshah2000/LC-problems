3b2d3bcb8d6d9cf3052a8755e188ba13dc8971e9
Swar Shah <55348140+swarshah2000@users.noreply.github.com>
2022-03-30 02:09:52 +0530

    Time: 5 ms (42.43%), Space: 6.8 MB (52.83%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -4,14 +4,20 @@ public:
         stack<char> s;
         string res;
         for (int i = 0; i < S.size(); i++){
-            if (!s.empty()) 
-                res += S[i];
-            if (S[i] == '(')
+            
+            if (S[i] == '('){
+                
+                if(s.size()>0 )
+                    res+=S[i];
                 s.push(S[i]);
-            else 
-                s.pop();
-            if (s.empty())
-                res.pop_back();
+            }
+            else{
+                 s.pop();
+                if(s.size()>0 )
+                    res+=S[i];
+            }
+           
+            
         }
         return res;
     }
